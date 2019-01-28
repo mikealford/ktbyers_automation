e10870b23a10a11bea65225afba73f66f5932d39
Mike Alford <mike.alford13@gmail.com>
2019-01-28 22:07:30 +0000

    Add week 4

---
+++ week4/paramiko_test2.py
@@ -0,0 +1,21 @@
+import time
+import paramiko
+from getpass import getpass
+
+ip_addr = '192.168.122.172'
+username = 'malford'
+password = getpass()
+port = 22
+
+remote_conn_pre = paramiko.SSHClient()
+remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+remote_conn_pre.connect(ip_addr, username=username, password=password, look_for_keys=False, allow_agent=False, port=port)
+
+remote_conn = remote_conn_pre.invoke_shell()
+remote_conn.settimeout(6.0)
+
+interfaces = remote_conn.send("show ip int brief\n")
+interfaces = remote_conn.recv(5000)
+
+print(interfaces)
