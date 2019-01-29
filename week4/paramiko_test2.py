bbbaf3c7855b966fe1914a885b81b1915b5e56de
Mike Alford <mike.alford13@gmail.com>
2019-01-29 03:16:13 +0000

    updated

--- week4/paramiko_test2.py
+++ week4/paramiko_test2.py
@@ -9,13 +9,13 @@ port = 22
 
 remote_conn_pre = paramiko.SSHClient()
 remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
 remote_conn_pre.connect(ip_addr, username=username, password=password, look_for_keys=False, allow_agent=False, port=port)
 
 remote_conn = remote_conn_pre.invoke_shell()
-remote_conn.settimeout(6.0)
 
-interfaces = remote_conn.send("show ip int brief\n")
-interfaces = remote_conn.recv(5000)
+remote_conn.send("show ip int br\n")
+time.sleep(1)
+outp = remote_conn.recv(5000)
+print(outp)
 
-print(interfaces)
+remote_conn.close
